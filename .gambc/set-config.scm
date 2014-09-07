504696e6f48236e6919b01d8f791fd54fb1d56cf
Christian Jaeger <ch@christianjaeger.ch>
2014-09-07 23:38:23 +0100

    compile. sigh. "does not" compile,
     6252 1108      20   0  704m 617m 4492 R  99.5 15.8   2:20.69 cc1
    
    cc1: out of memory allocating 4064 bytes after a total of 927133696 bytes
    *** ERROR IN #<procedure #2>, ".gambc/set-compiler.scm"@87.8 -- C compilation or link failed while compiling "vcard.scm"

--- .gambc/set-config.scm
+++ .gambc/set-config.scm
@@ -1,2 +1,2 @@
 (define compile-mode 'c) ;; s source, c compile, (i is currently buggy)
-(define compile-options '(options: (debug keep-c) cc-options: "-g"))
+(define compile-options '(options: () cc-options: "-g"))
